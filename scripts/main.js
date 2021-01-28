ba2a4217327750cd52f9142c889078cff425c05f
Loganwebb1994 <loganwebb1994@gmail.com>
2021-01-28 15:26:03 -0600

    added button to show noted that responds when clicked

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,11 @@ import {CriminalsList} from "./criminals/CriminalList.js"
 import {ConvictionSelect} from "./convictions/ConvictionSelect.js"
 import {OfficerSelect} from "./officers/OfficerSelect.js"
 import {NoteForm} from "./notes/NoteForm.js"
-import {ShowNoteButton} from "./notes/ShowNotesButton.js"
+
 
 NoteForm()
 OfficerList()
 CriminalsList()
 ConvictionSelect()
 OfficerSelect()
-ShowNoteButton()
+
